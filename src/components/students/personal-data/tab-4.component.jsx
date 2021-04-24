638f1807bef67700a8249554e3fc17846ae4bfe1
reyrey1616 <dabboyrey@gmail.com>
2021-04-24 18:33:42 +0800

    Fixed error on character reference form

--- src/components/students/personal-data/tab-4.component.jsx
+++ src/components/students/personal-data/tab-4.component.jsx
@@ -44,77 +44,76 @@ const CharacterReferences = ({ data, userType }) => {
 
 	return (
 		<div className="tab-page-container">
-			{userType !== "evaluator" ||
-				("admin" && (
-					<Form
-						form={form}
-						layout="vertical"
-						name="basic"
-						initialValues={{ remember: true }}
-						onFinish={onFinish}
-						onFinishFailed={onFinishFailed}
-					>
-						<div className="flex flex-wrap mb-2">
-							<div className="col-12 flex">
-								<Form.Item
-									className="col-4 col-md-12 p-half mb-0"
-									label="Full Name"
-									name="fullName"
-									rules={[
-										{
-											required: true,
-											message:
-												"Please input character reference name",
-										},
-									]}
+			{userType !== "evaluator" && userType !== "admin" && (
+				<Form
+					form={form}
+					layout="vertical"
+					name="basic"
+					initialValues={{ remember: true }}
+					onFinish={onFinish}
+					onFinishFailed={onFinishFailed}
+				>
+					<div className="flex flex-wrap mb-2">
+						<div className="col-12 flex">
+							<Form.Item
+								className="col-4 col-md-12 p-half mb-0"
+								label="Full Name"
+								name="fullName"
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input character reference name",
+									},
+								]}
+							>
+								<Input allowClear />
+							</Form.Item>
+							<Form.Item
+								className="col-3 col-md-12 p-half mb-0"
+								label="Position/Affliation"
+								name="position"
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input position/affliation!",
+									},
+								]}
+							>
+								<Input allowClear />
+							</Form.Item>
+							<Form.Item
+								className="col-3 col-md-12 p-half mb-0"
+								label="Contact No."
+								name="contactNumber"
+								rules={[
+									{
+										required: true,
+										message:
+											"Please input character reference contact number!",
+									},
+								]}
+							>
+								<Input allowClear />
+							</Form.Item>
+							<Form.Item
+								className="col-2 col-md-12 p-half mb-0"
+								label="Click here to save"
+							>
+								<Button
+									htmlType="submit"
+									type="primary"
+									size="large"
+									loading={buttonLoading}
 								>
-									<Input allowClear />
-								</Form.Item>
-								<Form.Item
-									className="col-3 col-md-12 p-half mb-0"
-									label="Position/Affliation"
-									name="position"
-									rules={[
-										{
-											required: true,
-											message:
-												"Please input position/affliation!",
-										},
-									]}
-								>
-									<Input allowClear />
-								</Form.Item>
-								<Form.Item
-									className="col-3 col-md-12 p-half mb-0"
-									label="Contact No."
-									name="contactNumber"
-									rules={[
-										{
-											required: true,
-											message:
-												"Please input character reference contact number!",
-										},
-									]}
-								>
-									<Input allowClear />
-								</Form.Item>
-								<Form.Item
-									className="col-2 col-md-12 p-half mb-0"
-									label="Click here to save"
-								>
-									<Button
-										htmlType="submit"
-										type="primary"
-										size="large"
-										loading={buttonLoading}
-									>
-										Add Entry
-									</Button>
-								</Form.Item>
-							</div>
+									Add Entry
+								</Button>
+							</Form.Item>
 						</div>
-					</Form>
-				))}
+					</div>
+				</Form>
+			)}
 
 			<div className="table-container mt-2">
 				<CharacterReferencesTable
