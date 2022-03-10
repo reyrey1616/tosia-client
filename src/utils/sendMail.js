9c08ea62daa2818847d7b64cc1bdef59ec0a9ca3
reyrey1616 <dabboyrey@gmail.com>
2022-03-10 13:59:31 +0800

    update the gmail service

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,25 +1,25 @@
 import emailjs from "emailjs-com";
 
 export const verificationEmail = ({ name, to_email, verification_link }) => {
-	emailjs
-		.send(
-			"service_c4j88ga",
-			"template_cx9upvf",
-			{
-				name,
-				from_name: "Executive Committee",
-				to_email,
-				verification_link,
-			},
-			"user_6IFNMA9CyTtMhVkdTlmFY"
-		)
+  emailjs
+    .send(
+      "service_27v20lv",
+      "template_cx9upvf",
+      {
+        name,
+        from_name: "Executive Committee",
+        to_email,
+        verification_link,
+      },
+      "user_6IFNMA9CyTtMhVkdTlmFY"
+    )
 
-		.then(
-			(result) => {
-				console.log(result.text);
-			},
-			(error) => {
-				console.log(error.text);
-			}
-		);
+    .then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
 };
