f47c24e6d451864756f432a2882282a5150e3e17
maamcelmac <princess.pojol@wvsu.edu.ph>
2022-04-11 13:09:59 +0800

    Edit login image

--- src/pages/students/login.page.jsx
+++ src/pages/students/login.page.jsx
@@ -10,85 +10,75 @@ import { notify } from "../../components/global/alerts/alerts.component";
 import { useHistory } from "react-router-dom";
 const token = localStorage.getItem("stkn");
 const LoginPage = () => {
-	const history = useHistory();
+  const history = useHistory();
 
-	useEffect(() => {
-		(async function loadStudent() {
-			if (token) {
-				setAuthToken(token);
-				try {
-					const request = await axios.get("/auth/get-student");
-					const response = request.data;
-					console.log(response.data);
-					if (response.success) {
-						notify(
-							"Login Success!",
-							"success",
-							"Welcome to TOSIA"
-						);
-						setTimeout(() => {
-							history.push("/student/personal-data");
-						}, 1500);
-					} else {
-						throw Error;
-					}
-				} catch (error) {
-						errorCatch(error, "Login Failed");
-				}
-			}
-		})();
-	}, []);
+  useEffect(() => {
+    (async function loadStudent() {
+      if (token) {
+        setAuthToken(token);
+        try {
+          const request = await axios.get("/auth/get-student");
+          const response = request.data;
+          console.log(response.data);
+          if (response.success) {
+            notify("Login Success!", "success", "Welcome to TOSIA");
+            setTimeout(() => {
+              history.push("/student/personal-data");
+            }, 1500);
+          } else {
+            throw Error;
+          }
+        } catch (error) {
+          errorCatch(error, "Login Failed");
+        }
+      }
+    })();
+  }, []);
 
-	const signUpRef = useRef(null);
-	const executeScroll = () => signUpRef.current.scrollIntoView();
+  const signUpRef = useRef(null);
+  const executeScroll = () => signUpRef.current.scrollIntoView();
 
-	return (
-		<div
-			className="flex bg-dirtywhite login-page-wrapper p-5"
-			style={{ height: "auto", minHeight: "100vh" }}
-		>
-			<div className="flex card bg-gradient login-page-container">
-				<div className="col-6 col-md-12 animate__animated animate__fadeInRightBig flex-column p-2 pl-6 pr-6 registration-image">
-					<img src={Image} alt="Tosia" />
-				</div>
-				<div
-					className="col-6 col-md-12 animate__animated animate__fadeInLeftBig bg-white"
-					style={{
-						borderTopRightRadius: "15px",
-						borderBottomRightRadius: "15px",
-					}}
-					ref={signUpRef}
-				>
-					<div
-						className="p-1 pr-3 pl-3 login-page-login-form"
-						style={{ justifyContent: "space-evenly" }}
-					>
-						<div>
-							<p className="text-body m-0 text-grey">
-								{" "}
-								WELCOME BACK!
-							</p>
-							<h2 className="text-title text-blue m-0 ">
-								Please login your account.
-							</h2>
-						</div>
+  return (
+    <div
+      className="flex bg-dirtywhite login-page-wrapper p-2"
+      style={{ height: "auto", minHeight: "100vh" }}
+    >
+      <div className="flex card bg-gradient login-page-container">
+        <div className="col-6 col-md-12 animate__animated animate__fadeInRightBig flex-column p-2 pl-6 pr-6 registration-image">
+          <img src={Image} alt="Tosia" />
+        </div>
+        <div
+          className="col-6 col-md-12 animate__animated animate__fadeInLeftBig bg-white"
+          style={{
+            borderTopRightRadius: "15px",
+            borderBottomRightRadius: "15px",
+          }}
+          ref={signUpRef}
+        >
+          <div
+            className="p-1 pr-3 pl-3 login-page-login-form"
+            style={{ justifyContent: "space-evenly" }}
+          >
+            <div>
+              <p className="text-body m-0 text-grey"> WELCOME BACK!</p>
+              <h2 className="text-title text-blue m-0 ">
+                Please login your account.
+              </h2>
+            </div>
 
-						<LoginForm />
+            <LoginForm />
 
-						<center>
-							<p className="text-body text-grey">
-								<label> DON'T HAVE AN ACCOUNT? </label>{" "}
-								<Link to="/registration">
-									{" "}
-									Click here to register
-								</Link>
-							</p>
-						</center>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <center>
+              <p className="text-body text-grey">
+                <label> DON'T HAVE AN ACCOUNT? </label>{" "}
+                <Link to="/registration"> Click here to register</Link>
+              </p>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
